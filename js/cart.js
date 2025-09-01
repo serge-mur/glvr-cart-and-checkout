bd978f061bb3007d84497ce2d6c9b6f7dc5e8fdd
Serge <serge-mur@yandex.ru>
2025-09-01 16:18:56 +0300

    scripts

--- js/cart.js
+++ js/cart.js
@@ -38,6 +38,36 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     });
 
+    // mob hide/show select
+    const cartTitleControl = document.querySelector('.cart__title-control');
+    const productsList = document.querySelector('.order:not(.order_not-available.products__list) .products__list');
+    let cartTitleControlText;
+    cartTitleControl.addEventListener("click", () => {         
+        productsList.classList.toggle('products__list_hide-select');
+        if(productsList.classList.contains('products__list_hide-select')) {
+            cartTitleControlText = 'Выбрать';
+        } else {
+            cartTitleControlText = 'Отменить';
+        }
+        cartTitleControl.textContent = cartTitleControlText;
+    }); 
+
+    // all select/unselect
+    const cartCheckboxInputAll = document.querySelector('.cart-checkbox__input_all');
+    const cartCheckboxInput = document.querySelectorAll('.order:not(.order_not-available.products__list) .products__item .cart-checkbox__input');
+    cartCheckboxInputAll.addEventListener('change', () => {
+        if(cartCheckboxInputAll.checked) {
+            cartCheckboxInput.forEach(checkbox => {
+                checkbox.checked = true;
+            });
+        } else {
+            cartCheckboxInput.forEach(checkbox => {
+                checkbox.checked = false;
+            });
+        }
+
+    });
+
     // size dropdown
     document.querySelectorAll('.size-dropdown__button').forEach(btn => {
         const sizeSelect = btn.closest('.products__item').querySelector('.size-select');
@@ -50,26 +80,52 @@ document.addEventListener("DOMContentLoaded", () => {
     });
 
     // product counter
-    const counterMinus = document.querySelector('.counter__minus');
-    const counterPlus = document.querySelector('.counter__plus');
-    const counterValue = document.querySelector('.counter__value');
-    counterMinus.addEventListener('click', () => {
-        let currentValue = parseInt(counterValue.value);
-        if (currentValue > 1) {
-            counterValue.value = currentValue - 1;
+    document.querySelectorAll('.products__counter').forEach(counter => {
+        const counterMinus = counter.querySelector('.counter__minus');
+        const counterPlus = counter.querySelector('.counter__plus');
+        const counterValue = counter.querySelector('.counter__value');
+        const productsItem = counter.closest('.products__item');
+        if (parseInt(counterValue.value) == 1) {
+            counterMinus.classList.add('counter__minus_delete');   
         }
-    });
-    counterPlus.addEventListener('click', () => {
-        let currentValue = parseInt(counterValue.value);
-        counterValue.value = currentValue + 1;
-    });
-    counterValue.addEventListener('change', () => {
-        let currentValue = parseInt(counterValue.value);
-        if (isNaN(currentValue) || currentValue < 1) {
-            counterValue.value = 1; // Установить значение по умолчанию, если ввод некорректный
-            counterMinus.classList.add('counter__minus_delete');
-        }
-    });
+        counterMinus.addEventListener('click', () => {
+            if (counterMinus.classList.contains('counter__minus_delete')) {
+                productsItem.classList.add('products__item_delete');
+                // delete product counter
+                let time = 4;
+                const timer = setInterval(() => {
+                    const countdownElement = productsItem.querySelector('.delete-counter__counter');
+                    if(time > 0) {
+                        countdownElement.textContent = time--;
+                    } else {
+                        clearInterval(timer)
+                        countdownElement.textContent = '0';
+                        console.log('Удаляем товар')                        
+                      }
+                }, 1000);
+            }
+            if (parseInt(counterValue.value) == 2) {
+                counterMinus.classList.add('counter__minus_delete');
+            }
+            let currentValue = parseInt(counterValue.value);
+            if (currentValue > 1) {
+                counterValue.value = currentValue - 1 + ' шт';
+            }
+        });
+        counterPlus.addEventListener('click', () => {
+            let currentValue = parseInt(counterValue.value);
+            counterValue.value = currentValue + 1+ ' шт';
+            if (counterMinus.classList.contains('counter__minus_delete')) {
+                counterMinus.classList.remove('counter__minus_delete');
+            }
+        });
+        counterValue.addEventListener('change', () => {
+            let currentValue = parseInt(counterValue.value);
+            if (isNaN(currentValue) || currentValue < 1) {
+                counterValue.value = 1;
 
+            }
+        });
+    });
 
 });
